c92b5b68898dcf7d49524ef0902d9bbc30a8dca3
Randy Fay <randy@randyfay.com>
2024-06-10 15:21:11 -0600

    Improve push builder

--- image/push.sh
+++ image/push.sh
@@ -3,6 +3,9 @@
 set -eu -o pipefail
 DOCKER_IMAGE=ddev/mysql-client-build
 
-docker buildx create --name cmake --use 2>/dev/null || true
+set -x
+
+docker buildx create --name cmake-builder 2>/dev/null || true
+docker buildx use cmake-builder
 
 docker buildx build --push --platform linux/amd64,linux/arm64 -t ${DOCKER_IMAGE} .
